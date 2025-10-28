b3abf50c7d1871a02be32461359a375b561f0685
Ruwanthamadushan17 <ruwantha.lankeshwara@gmail.com>
2025-10-28 22:47:34 +0000

    Updated trips-list.component template

--- travel-planner-web/src/app/app.routes.ts
+++ travel-planner-web/src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'trips', pathMatch: 'full' },
-    { path: 'trips', loadComponent: () => import('./features/trips/trips-list.component').then(m => m.TripsListComponent) },
+    { 
+        path: 'trips', 
+        loadComponent: () => import('./features/trips/trips-list.component')
+            .then(m => m.TripsListComponent) },
 ];
